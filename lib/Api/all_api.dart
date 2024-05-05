f83cf287007bccde26c77633df9fe2735794bdcc
lkrjangid1 <lkrjangid1@gmail.com>
2024-05-05 13:43:18 +0530

    v1.1.4+12

--- lib/Api/all_api.dart
+++ lib/Api/all_api.dart
@@ -3,6 +3,10 @@ class AllApi {
 
   static const baseUrl1  = 'https://www.zalphabrains.in';
 
+  // static const baseUrl  = 'http://20.42.87.219/api';
+
+  // static const baseUrl1  = 'http://20.42.87.219';
+
   //register user
   static const registerUser = "$baseUrl/register/user/";
 
